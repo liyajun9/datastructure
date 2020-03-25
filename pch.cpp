ef335c6ebf3003fefe2955f9303f669122974b43
ralphLina <ralph@localhost.localdomain>
2020-03-25 21:52:57 +0800

    init add

---
+++ pch.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ralph on 3/25/20.
+//
+#include "pch.h"
+
---
+++ pch.h
@@ -0,0 +1,11 @@
+//
+// Created by ralph on 3/25/20.
+//
+
+#ifndef DATASTRUCTURE_PCH_H
+#define DATASTRUCTURE_PCH_H
+
+#include <iostream>
+#include <string>
+
+#endif //DATASTRUCTURE_PCH_H
